73e6d554b301d0c05cbbab0c381cd895d1c1b8d8
katan <hashby.alhadi@gmail.com>
2025-03-15 03:23:30 +0700

    Remove WeatherView and its binding from app routing

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
-import '../modules/home/weather/bindings/weather_binding.dart';
-import '../modules/home/weather/views/weather_view.dart';
 
 part 'app_routes.dart';
 
@@ -17,13 +15,6 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
-      children: [
-        GetPage(
-          name: _Paths.WEATHER,
-          page: () => const WeatherView(),
-          binding: WeatherBinding(),
-        ),
-      ],
     ),
   ];
 }
