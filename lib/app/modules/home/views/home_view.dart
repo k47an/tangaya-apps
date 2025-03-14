e82c6649fafb39d2fcc9554a8eb1f4038c5bc6b8
katan <hashby.alhadi@gmail.com>
2025-03-15 03:23:12 +0700

    Refactor HomeView to use HeaderWidget and WeatherWidget for improved layout

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tangaya_apps/app/modules/home/weather/views/weather_view.dart';
-import 'package:tangaya_apps/app/modules/home/weather/controllers/weather_controller.dart';
-
+import 'package:tangaya_apps/app/modules/home/views/widgets/header_widget.dart';
+import 'package:tangaya_apps/app/modules/home/views/widgets/weather_widget.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,16 +9,29 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
-      body: Column(
-        children: [
-          GetBuilder<WeatherController>(
-            init: WeatherController(),
-            builder: (controller) {
-              return WeatherView();
-            },
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            HeaderWidget(),
+            Positioned(top: 150, left: 0, right: 0, child: WeatherWidget()),
+            // Positioned(
+            //   top: 300,
+            //   left: 0,
+            //   right: 0,
+            //   child: Container(
+            //     padding: EdgeInsets.all(20),
+            //     decoration: BoxDecoration(
+            //       color: Colors.white,
+            //       borderRadius: BorderRadius.only(
+            //         topLeft: Radius.circular(30),
+            //         topRight: Radius.circular(30),
+            //       ),
+            //     ),
+            //     child: Column(children: [Text('Content goes here')]),
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
