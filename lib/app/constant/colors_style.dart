a80b4e94ca50e0e593af541b35e8498f6c1965e1
katan <hashby.alhadi@gmail.com>
2025-03-15 03:21:48 +0700

    Add color and text style constants, and implement app theme configuration

---
+++ lib/app/constant/colors_style.dart
@@ -0,0 +1,35 @@
+part of 'constant.dart';
+
+class Primary {
+  static const Color mainColor = Color(0xFF499448);
+  static const Color lightColor = Color(0xFFA3D977);
+  static const Color darkColor = Color(0xFF1B5E20);
+  static const Color subtleColor = Color(0xFFE8F5E9);
+}
+
+class Error {
+  static const Color mainColor = Color(0xFFD32F2F);
+  static const Color lightColor = Color(0xFFFF6659);
+  static const Color darkColor = Color(0xFF9A0007);
+  static const Color subtleColor = Color(0xFFFFEBEE);
+}
+
+class Warning {
+  static const Color mainColor = Color(0xFFFBC02D);
+  static const Color lightColor = Color(0xFFFFF263);
+  static const Color darkColor = Color(0xFFC49000);
+  static const Color subtleColor = Color(0xFFFFF9C4);
+}
+
+class Neutral {
+  static const Color dark1 = Color(0xFF212121);
+  static const Color dark2 = Color(0xFF424242);
+  static const Color dark3 = Color(0xFF616161);
+  static const Color dark4 = Color(0xFF757575);
+  static const Color dark5 = Color(0xFF9E9E9E);
+  static const Color white1 = Color(0xFFFAFAFA);
+  static const Color white2 = Color(0xFFF5F5F5);
+  static const Color white3 = Color(0xFFEEEEEE);
+  static const Color white4 = Color(0xFFE0E0E0);
+  static const Color transparent = Colors.transparent;
+}
---
+++ lib/app/constant/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+part 'colors_style.dart';
+part 'text_style.dart';
