d24c621d4409980e33fa9a03ab0cacca04d5f1b2
katan <hashby.alhadi@gmail.com>
2025-03-18 02:01:38 +0700

    Enhance WeatherWidget with background weather icon and updated styling

--- lib/app/modules/home/views/widgets/weather_widget.dart
+++ lib/app/modules/home/views/widgets/weather_widget.dart
@@ -8,93 +8,127 @@ class WeatherWidget extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      child: Obx(() {
-        final weather = controller.currentWeather.value;
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Temp',
-                  style: light.copyWith(color: Neutral.dark1, fontSize: 12),
+    return Obx(() {
+      final weather = controller.currentWeather.value;
+      return Stack(
+        children: [
+          if (weather?.weatherIcon != null)
+            Positioned(
+              top: -20,
+              bottom: 0,
+              left: -100,
+              right: 100,
+              child: Opacity(
+                opacity: 0.1,
+                child: Center(
+                  child: Image.network(
+                    "https://openweathermap.org/img/wn/${weather?.weatherIcon}.png",
+                    height: 400,
+                    width: 400,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                Container(
-                  width: 80,
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Stack(
-                    children: [
-                      Text(
-                        weather?.temperature?.celsius?.toStringAsFixed(0) ??
-                            '0',
-                        style: extraBold.copyWith(
-                          color: Neutral.dark1,
-                          fontSize: 50,
-                        ),
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 30,
+              top: 20,
+              right: 30,
+              bottom: 40,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Temp',
+                      style: light.copyWith(
+                        color: Primary.subtleColor,
+                        fontSize: 12,
                       ),
-                      Positioned(
-                        top: -10,
-                        left: 53,
-                        child: Text(
-                          '°c',
-                          style: extraBold.copyWith(
-                            color: Neutral.dark1,
-                            fontSize: 50,
+                    ),
+                    Container(
+                      width: 80,
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Stack(
+                        children: [
+                          Text(
+                            weather?.temperature?.celsius?.toStringAsFixed(0) ??
+                                '0',
+                            style: extraBold.copyWith(
+                              color: Primary.subtleColor,
+                              fontSize: 50,
+                            ),
                           ),
+                          Positioned(
+                            top: -10,
+                            left: 53,
+                            child: Text(
+                              '°c',
+                              style: extraBold.copyWith(
+                                color: Primary.subtleColor,
+                                fontSize: 50,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      child: Text(
+                        '${(weather?.tempMin?.celsius?.toDouble() ?? 0).round()}°  /  ${(weather?.tempMax?.celsius?.toDouble() ?? 0).round()}°',
+                        style: light.copyWith(
+                          color: Primary.subtleColor,
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Cuaca terkini',
+                            style: regular.copyWith(
+                              color: Primary.subtleColor,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            'POKDARWIS Tangaya',
+                            style: regular.copyWith(
+                              color: Primary.subtleColor,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      Text(
+                        weather?.weatherDescription ?? 'Tidak tersedia',
+                        style: bold.copyWith(
+                          color: Primary.subtleColor,
+                          fontSize: 16,
                         ),
                       ),
                     ],
                   ),
                 ),
-                Container(
-                  width: 90,
-                  child: Text(
-                    '${weather?.tempMin?.celsius?.toStringAsFixed(0) ?? '0'}°  /  ${weather?.tempMax?.celsius?.toStringAsFixed(0) ?? '0'}°',
-                    style: light.copyWith(color: Neutral.dark1, fontSize: 12),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
               ],
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Cuaca terkini',
-                      style: regular.copyWith(
-                        color: Neutral.dark1,
-                        fontSize: 10,
-                      ),
-                    ),
-                    Text(
-                      'POKDARWIS Tangaya',
-                      style: regular.copyWith(
-                        color: Neutral.dark1,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-                Image.network(
-                  'http://openweathermap.org/img/w/${weather?.weatherIcon ?? '01d'}.png',
-                  height: 50,
-                  width: 50,
-                  fit: BoxFit.cover,
-                ),
-                Text(
-                  weather?.weatherDescription ?? 'Tidak tersedia',
-                  style: extraBold.copyWith(color: Neutral.dark1, fontSize: 16),
-                ),
-              ],
-            ),
-          ],
-        );
-      }),
-    );
+          ),
+        ],
+      );
+    });
   }
 }
