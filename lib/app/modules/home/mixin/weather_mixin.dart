c3ea2beff18b539af82b631fc5ecacf0676e470d
katan <hashby.alhadi@gmail.com>
2025-03-18 02:00:54 +0700

    Implement periodic weather fetching in WeatherMixin

--- lib/app/modules/home/mixin/weather_mixin.dart
+++ lib/app/modules/home/mixin/weather_mixin.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:get/get.dart';
 import 'package:tangaya_apps/app/data/api/api.dart';
 import 'package:weather/weather.dart';
@@ -12,9 +12,25 @@ mixin WeatherMixin on GetxController {
   final Rx<Weather?> currentWeather = Rx<Weather?>(null);
   final double latitude = -0.708490224747233;
   final double longitude = 100.53330084137643;
+  Timer? _timer;
 
   void fetchCurrentWeather() async {
     Weather weather = await wf.currentWeatherByLocation(latitude, longitude);
     currentWeather.value = weather;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchCurrentWeather();
+    _timer = Timer.periodic(Duration(hours: 1), (timer) {
+      fetchCurrentWeather();
+    });
+  }
+
+  @override
+  void onClose() {
+    _timer?.cancel();
+    super.onClose();
   }
 }
