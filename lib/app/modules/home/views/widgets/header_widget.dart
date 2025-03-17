80aa656a3827ce1e110619eb6dd4e86f08da7b66
katan <hashby.alhadi@gmail.com>
2025-03-18 02:01:44 +0700

    Refactor HeaderWidget to enhance layout and styling with AppBar and improved profile display

--- lib/app/modules/home/views/widgets/header_widget.dart
+++ lib/app/modules/home/views/widgets/header_widget.dart
@@ -7,57 +7,84 @@ class HeaderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
       decoration: BoxDecoration(
-        color: Primary.mainColor,
-        gradient: LinearGradient(
-          colors: [Primary.mainColor, Primary.subtleColor, Neutral.white1],
-          stops: [0.2, 0.9, 1.0],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      height: 500,
-      padding: EdgeInsets.only(top: 60, left: 20, right: 20),
-      alignment: Alignment.topCenter,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/dummy/profile.JPG'),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(50),
-            ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
           ),
-          SizedBox(width: 10),
-          Column(
+        ],
+      ),
+      child: AppBar(
+        backgroundColor: Primary.darkColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        ),
+        toolbarHeight: 70,
+        flexibleSpace: Padding(
+          padding: EdgeInsets.only(top: 60, left: 10, right: 10, bottom: 20),
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Selamat Datang',
-                style: extraBold.copyWith(color: Neutral.dark1, fontSize: 16),
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Primary.lightColor, width: 1),
+                  image: DecorationImage(
+                    image: AssetImage('assets/dummy/profile.JPG'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
               ),
-              Text(
-                'Katan',
-                style: regular.copyWith(color: Neutral.dark1, fontSize: 14),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Selamat Datang',
+                    style: extraBold.copyWith(
+                      color: Primary.subtleColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'Katan',
+                    style: regular.copyWith(
+                      color: Primary.subtleColor,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.notifications),
+                    color: Primary.subtleColor,
+                    iconSize: 25,
+                    onPressed: () {
+                      // Navigate to search page
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.message_rounded),
+                    color: Primary.subtleColor,
+                    iconSize: 25,
+                    onPressed: () {
+                      // Navigate to search page
+                    },
+                  ),
+                ],
               ),
             ],
           ),
-          Spacer(),
-          IconButton(
-            icon: Icon(Icons.notifications),
-            color: Neutral.dark1,
-            iconSize: 25,
-            onPressed: () {
-              // Navigate to search page
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
